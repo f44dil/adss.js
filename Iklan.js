3518deeddd8599bcf7bd1daa9d0d0fe90ddff727
f44dil <31163006+f44dil@users.noreply.github.com>
2017-12-09 09:30:14 +0700

    Create Iklan.js

---
+++ Iklan.js
@@ -0,0 +1,21 @@
+<script type="text/javascript">
+var imgs1 = new Array("http://s5.postimg.org/rcnwde7qv/image.gif","http://xpanas.com/wp-content/uploads/2017/10/skandal-spg-rokok.png","https://1.bp.blogspot.com/-AqytH6XrETI/WeQU3ZA3a4I/AAAAAAAAABw/ExzWkAtxtMg3aUnc9Rm-REsUIbpo5UH2gCLcBGAs/s1600/vimax.gif","http://xpanas.com/wp-content/uploads/2017/04/jilbab-sange.png");
+var lnks1 = new Array("http://shtme.co/direct/111937","http://shtme.co/direct/111937","http://shtme.co/direct/111937","http://shtme.co/direct/111937");
+var alt1 = new Array("Tempat Download Bokep","Anak Di Perkosa Ayah Tiri","Mertua Kejam","Anak Umur 15 tahun di Perkosa 2 Pria");
+var currentAd1 = 0;
+var imgCt1 = 4;
+function cycle1() {
+if (currentAd1 == imgCt1) {
+currentAd1 = 0;
+}
+var banner1 = document.getElementById('adBanner1');
+var link1 = document.getElementById('adLink1');
+banner1.src=imgs1[currentAd1]
+banner1.alt=alt1[currentAd1]
+document.getElementById('adLink1').href=lnks1[currentAd1]
+currentAd1++;
+}
+window.setInterval("cycle1()",4000);
+</script>
+<a href="http://shtme.co/direct/111937" id="adLink1" target="_blank">
+<img src="http://ulux.mobie.in/img/d2.gif" id="adBanner1" border="0" width="250" height="100" /></a>
